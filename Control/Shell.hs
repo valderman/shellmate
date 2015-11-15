d6de9133f7d346d5d458c019cc8a8036b46d9f0a
Anton Ekblad <anton@ekblad.cc>
2015-11-15 16:22:02 +0100

    Refactor into internal/external parts.

--- Control/Shell.hs
+++ Control/Shell.hs
@@ -3,265 +3,88 @@
              CPP,
              UndecidableInstances #-}
 -- | Simple interface for shell scripting-like tasks.
-module Control.Shell ( 
+module Control.Shell (
+    -- * Basic types and classes
     Shell,
+
+    -- * Running Shell programs
+    shell, (|>),
+
+    -- * Error handling
     Guard (..),
-    File (..),
-    shell,
-    mayFail, orElse,
-    withEnv, getEnv, lookupEnv,
-    run, run_, runInteractive, genericRun, sudo,
+    try, orElse,
+
+    -- * Environment handling
+    setEnv, getEnv, withEnv, lookupEnv,
+
+    -- * Running commands
+    MonadIO (..),
+    run, run_, runInteractive, sudo,
+
+    -- * Working with directories
     cd, cpDir, pwd, ls, mkdir, rmdir, inDirectory, isDirectory,
     withHomeDirectory, inHomeDirectory, withAppDirectory, inAppDirectory,
-    forEachFile, forEachFile_, cpFiltered,
-    isFile, rm, mv, cp,
+    forEachFile, forEachFile_,
+
+    -- * Working with files
+    isFile, rm, mv, cp, readf, writef,
+
+    -- * Working with temporary files and directories
     withTempFile, withCustomTempFile,
     withTempDirectory, withCustomTempDirectory, inTempDirectory,
+
+    -- * Text I/O
     hPutStr, hPutStrLn, hClose, echo,
-    (|>),
-    module System.FilePath, liftIO
+
+    -- * @FilePath@s
+    module System.FilePath
   ) where
 import Control.Applicative
 import Control.Monad (ap, forM, filterM, forM_, when)
-import Control.Monad.IO.Class
 import Data.Time.Clock
-import Data.Typeable
 import System.FilePath
 import System.IO.Unsafe
-import qualified System.Process as Proc
 import qualified System.Directory as Dir
-import qualified System.Exit as Exit
-import qualified System.IO as IO
-import qualified Control.Exception as Ex
-import qualified Control.Concurrent as Conc
-import qualified System.IO.Temp as Temp
 import qualified System.Environment as Env
+import qualified System.IO as IO
+import Control.Shell.Internal
 
-newtype ShellException = ShellException String deriving (Typeable, Show)
-instance Ex.Exception ShellException
+-- | Lazily read a file.
+readf :: FilePath -> Shell String
+readf = liftIO . readFile
 
--- | Environment variables.
-type Env = [(String, String)]
+-- | Lazily write a file.
+writef :: FilePath -> String -> Shell ()
+writef f = liftIO . writeFile f
 
--- | A command name plus a ProcessHandle.
-data Pid = Pid {pidName :: String, pidHandle :: Proc.ProcessHandle}
+-- | Set an environment variable.
+setEnv :: MonadIO m => String -> String -> m ()
+setEnv k v = liftIO $ Env.setEnv k v
 
--- | Monad for running shell commands. If a command fails, the entire
---   computation is aborted unless @mayFail@ is used.
-newtype Shell a = Shell {
-    unSh :: Env -> IO (Either String a, [Pid])
-  }
-
-instance Monad Shell where
-  fail err = Shell $ \_ -> return (Left err, [])
-  return x = Shell $ \_ -> return (Right x, [])
-  -- | The bind operation of the Shell monad is effectively a barrier; all
-  --   commands on the left hand side of a bind will complete before any
-  --   command on the right hand side is attempted.
-  --   To lazily stream data between two commands, use the @|>@ combinator.
-  (Shell m) >>= f = Shell $ \env -> do
-    (x, pids) <- m env
-    merr <- waitPids pids
-    case (x, merr) of
-      (Left err, _) -> return (Left err, [])
-      (_, Just err) -> return (Left err, [])
-      (Right x', _) -> unSh (f x') env
-
-instance MonadIO Shell where
-  liftIO act = Shell $ \_ -> flip Ex.catch exHandler $ do
-    x <- act
-    return (Right x, [])
-
-instance Applicative Shell where
-  pure  = return
-  (<*>) = ap
-
-instance Functor Shell where
-  fmap f x = do
-    x' <- x
-    return $! f x'
-
--- | Enable the user to use 'file "foo" |> fmap reverse |> file "bar"' as
---   syntax for reading the file 'foo', reversing the contents and writing the
---   result to 'bar'.
---   Note that this uses lazy IO in the form of read/writeFile.
-class File a where
-  file :: FilePath -> a
-
-instance File (String -> Shell ()) where
-  file f = liftIO . writeFile f
-
-instance File (Shell String) where
-  file f = liftIO $ readFile f
-
--- | Lazy counterpart to monadic bind. To stream data from a command 'a' to a
---   command 'b', do 'a |> b'.
-(|>) :: Shell String -> (String -> Shell a) -> Shell a
-(Shell m) |> f = Shell $ \env -> do
-  (x, pids) <- m env
-  (x', pids') <- case x of
-    Left err -> return (Left err, [])
-    Right x' -> unSh (f x') env
-  return (x', pids ++ pids')
-infixl 1 |>
-
--- | Wait for all processes in the given list. If a process has failed, its
---   error message is returned and the rest are killed.
-waitPids :: [Pid] -> IO (Maybe String)
-waitPids (p:ps) = do
-  exCode <- Proc.waitForProcess (pidHandle p)
-  case exCode of
-    Exit.ExitFailure ec -> do
-      killPids ps
-      return . Just $ "Command '" ++ (pidName p) ++ "' failed with error "
-                    ++" code " ++ show ec
-    _ -> do
-      waitPids ps
-waitPids _ = do
-  return Nothing
-
--- | Kill all processes in the list.
-killPids :: [Pid] -> IO ()
-killPids = mapM_ (Proc.terminateProcess . pidHandle)
-
--- | Run a Shell computation. The program's working directory  will be restored 
---   after executing the computation.
-shell :: Shell a -> IO (Either String a)
-shell act = do
-  dir <- Dir.getCurrentDirectory
-  env <- Env.getEnvironment
-  (res, pids) <- unSh act env
-  merr <- waitPids pids
-  Dir.setCurrentDirectory dir
-  case merr of
-    Just err -> return $ Left err
-    _        -> return res
+-- | Get the value of an environment variable. Returns Nothing if the variable 
+--   doesn't exist.
+lookupEnv :: MonadIO m => String -> m (Maybe String)
+lookupEnv = liftIO . Env.lookupEnv
 
 -- | Run a computation with a new value for an environment variable.
 --   Note that this will *not* affect external commands spawned using @liftIO@
 --   or which directory is considered the system temp directory.
-withEnv :: String -> (String -> String) -> Shell a -> Shell a
-withEnv key f (Shell act) = Shell $ \env -> do
-    act $ insert env
-  where
-    insert (x@(k,v):xs) | k == key  = (key, f v) : xs
-                        | otherwise = x : insert xs
-    insert _                        = [(key, f "")]
-
--- | Get the value of an environment variable. Returns Nothing if the variable 
---   doesn't exist.
-lookupEnv :: String -> Shell (Maybe String)
-lookupEnv key = Shell $ \env -> return (Right $ lookup key env, [])
+withEnv :: MonadIO m => String -> (String -> String) -> m a -> m a
+withEnv key f act = do
+  v <- lookupEnv key
+  setEnv key $ f (maybe "" id v)
+  x <- act
+  maybe (return ()) (setEnv key) v
+  return x
 
 -- | Get the value of an environment variable. Returns the empty string if
 --   the variable doesn't exist.
-getEnv :: String -> Shell String
+getEnv :: MonadIO m => String -> m String
 getEnv key = maybe "" id `fmap` lookupEnv key
-
--- | General exception handler; any exception causes failure.
-exHandler :: Ex.SomeException -> IO (Either String a, [Pid])
-exHandler x = return (Left $ show x, [])
-
--- | Execute an external command. No globbing, escaping or other external shell
---   magic is performed on either the command or arguments. The program's text
---   output will be returned, and not echoed to the screen.
-run :: FilePath -> [String] -> String -> Shell String
-run p args stdin = do
-  (Just inp, Just out, pid) <- runP p args Proc.CreatePipe Proc.CreatePipe
-  Shell $ \_ -> do
-    let feed str = do
-          case splitAt 4096 str of
-            ([], [])      -> IO.hClose inp
-            (first, str') -> IO.hPutStr inp first >> feed str'
-    Conc.forkIO $ feed stdin
-    s <- IO.hGetContents out
-    s `seq` return (Right s, [Pid p pid])
-
--- | Run a program and return a boolean indicating whether the command
---   succeeded, the output from stdout, and the output from stderr.
---   This command will never fail.
-genericRun :: FilePath -> [String] -> String -> Shell (Bool, String, String)
-genericRun p args stdin = do
-    (Just inp, Just out, Just err, pid) <- createproc
-    Shell $ \_ -> do
-      let feed str = do
-            case splitAt 4096 str of
-              ([], [])      -> IO.hClose inp
-              (first, str') -> IO.hPutStr inp first >> feed str'
-      Conc.forkIO $ feed stdin
-      o <- IO.hGetContents out
-      e <- IO.hGetContents err
-      merr <- waitPids [Pid p pid]
-      return (Right (maybe True (const False) merr, o, e), [])
-  where
-    createproc = Shell $ \env -> do
-      (inp, out, err, pid) <- Proc.createProcess (cproc env)
-      return (Right (inp, out, err, pid), [])
-    cproc env = Proc.CreateProcess {
-        Proc.cmdspec      = Proc.RawCommand p args,
-        Proc.cwd          = Nothing,
-        Proc.env          = Just env,
-        Proc.std_in       = Proc.CreatePipe,
-        Proc.std_out      = Proc.CreatePipe,
-        Proc.std_err      = Proc.CreatePipe,
-        Proc.close_fds    = False,
-#if MIN_VERSION_process(1,2,0)
-        Proc.delegate_ctlc = False,
-#endif
-        Proc.create_group = False
-      }
-
-
--- | Like @run@, but echoes the command's text output to the screen instead of
---   returning it.
-run_ :: FilePath -> [String] -> String -> Shell ()
-run_ p args stdin = do
-  (Just inp, _, pid) <- runP p args Proc.CreatePipe Proc.Inherit
-  exCode <- liftIO $ do
-    IO.hPutStr inp stdin
-    IO.hClose inp
-    Proc.waitForProcess pid
-  case exCode of
-    Exit.ExitFailure ec -> fail $ "Command '" ++ p ++ "' failed with error " 
-                                ++" code " ++ show ec
-    _                   -> return ()
 
 -- | Run a command with elevated privileges.
 sudo :: FilePath -> [String] -> String -> Shell String
 sudo cmd args stdin = run "sudo" (cmd:args) stdin
-
--- | Create a process. Helper for @run@ and friends.
-runP :: String
-     -> [String]
-     -> Proc.StdStream
-     -> Proc.StdStream
-     -> Shell (Maybe IO.Handle, Maybe IO.Handle, Proc.ProcessHandle)
-runP p args stdin stdout = Shell $ \env -> do
-    (inp, out, _, pid) <- Proc.createProcess (cproc env)
-    return (Right (inp, out, pid), [])
-  where
-    cproc env = Proc.CreateProcess {
-        Proc.cmdspec      = Proc.RawCommand p args,
-        Proc.cwd          = Nothing,
-        Proc.env          = Just env,
-        Proc.std_in       = stdin,
-        Proc.std_out      = stdout,
-        Proc.std_err      = Proc.Inherit,
-        Proc.close_fds    = False,
-#if MIN_VERSION_process(1,2,0)
-        Proc.delegate_ctlc = False,
-#endif
-        Proc.create_group = False
-      }
-
--- | Run an interactive process.
-runInteractive :: FilePath -> [String] -> Shell ()
-runInteractive p args = do
-  (_, _, pid) <- runP p args Proc.Inherit Proc.Inherit
-  exitCode <- liftIO $ Proc.waitForProcess pid
-  case exitCode of
-    Exit.ExitFailure ec -> fail (show ec)
-    _                   -> return ()
 
 -- | Change working directory.
 cd :: FilePath -> Shell ()
@@ -296,21 +119,6 @@ cpDir from to = do
         else do
           liftIO $ Dir.createDirectoryIfMissing False to
           ls from >>= mapM_ (\f -> cpDir (from </> f) (to </> f))
-
--- | Recursively copy a directory, but omit all files that do not match the
---   give predicate.
-cpFiltered :: (FilePath -> Bool) -> FilePath -> FilePath -> Shell ()
-cpFiltered pred from to = do
-  isdir <- isDirectory to
-  -- Lazily create directories only when needed!
-  let to' = unsafePerformIO $ do
-        when (not isdir) $ Dir.createDirectory to
-        return to
-  files <- ls from
-  mapM_ ((`cp` to') . (from </>)) (filter pred files)
-  fromdirs <- filterM (\d -> isDirectory (from </> d)) files
-  forM_ fromdirs $ \dir -> do
-    cpFiltered pred (from </> dir) (to </> dir)
 
 -- | Perform an action on each file in the given directory.
 --   This function will traverse any subdirectories of the given as well.
@@ -394,58 +202,16 @@ isDirectory = liftIO . Dir.doesDirectoryExist
 isFile :: FilePath -> Shell Bool
 isFile = liftIO . Dir.doesFileExist
 
--- | Create a temp directory in the standard system temp directory, do
---   something with it, then remove it.
-withTempDirectory :: String -> (FilePath -> Shell a) -> Shell a
-withTempDirectory template act = Shell $ \env -> do
-    Temp.withSystemTempDirectory template (act' env)
-  where
-    act' env fp = Ex.catch (unSh (act fp) env) exHandler
-
--- | Create a temp directory in given directory, do something with it, then
---   remove it.
-withCustomTempDirectory :: FilePath -> (FilePath -> Shell a) -> Shell a
-withCustomTempDirectory dir act = Shell $ \env -> do
-    Temp.withTempDirectory dir "shellmate" (act' env)
-  where
-    act' env fp = Ex.catch (unSh (act fp) env) exHandler
-
 -- | Performs a command inside a temporary directory. The directory will be
 --   cleaned up after the command finishes.
 inTempDirectory :: Shell a -> Shell a
 inTempDirectory = withTempDirectory "shellmate" . flip inDirectory
 
--- | Create a temp file in the standard system temp directory, do something
---   with it, then remove it.
-withTempFile :: String -> (FilePath -> IO.Handle -> Shell a) -> Shell a
-withTempFile template act = Shell $ \env -> do
-    Temp.withSystemTempFile template (act' env)
-  where
-    act' env fp h = Ex.catch (unSh (act fp h) env) exHandler
-
--- | Create a temp file in the standard system temp directory, do something
---   with it, then remove it.
-withCustomTempFile :: FilePath -> (FilePath -> IO.Handle -> Shell a) -> Shell a
-withCustomTempFile dir act = Shell $ \env -> do
-    Temp.withTempFile dir "shellmate" (act' env)
-  where
-    act' env fp h = Ex.catch (unSh (act fp h) env) exHandler
-
--- | Perform an action that may fail without aborting the entire computation.
---   Forces serialization.
-mayFail :: Shell a -> Shell (Either String a)
-mayFail (Shell act) = Shell $ \env -> do
-  (x, pids) <- Ex.catch (act env) exHandler
-  merr <- waitPids pids
-  case merr of
-    Just err -> return (Right (Left err), [])
-    _        -> return (Right x, [])
-
 -- | Attempt to run the first command. If the first command fails, run the
 --   second. Forces serialization of the first command.
 orElse :: Shell a -> Shell a -> Shell a
 orElse a b = do
-  ex <- mayFail a
+  ex <- try a
   case ex of
     Right x -> return x
     _       -> b
---
+++ Control/Shell/Internal.hs
@@ -0,0 +1,221 @@
+{-# LANGUAGE CPP #-}
+module Control.Shell.Internal (
+    MonadIO (..), Shell,
+    shell, shell_,
+    (|>),
+    run, run_, runInteractive,
+    withTempDirectory, withCustomTempDirectory,
+    withTempFile, withCustomTempFile,
+    try
+  ) where
+import Control.Monad (ap, forM, filterM, forM_, when)
+import Control.Monad.IO.Class
+import Data.Typeable
+import qualified Control.Concurrent as Conc
+import qualified Control.Exception as Ex
+import qualified Data.Map as M
+import qualified System.Directory as Dir
+import qualified System.Environment as Env
+import qualified System.Exit as Exit
+import qualified System.Process as Proc
+import qualified System.IO as IO
+import qualified System.IO.Temp as Temp
+
+newtype ShellException = ShellException String deriving (Typeable, Show)
+instance Ex.Exception ShellException
+
+-- | A command name plus a ProcessHandle.
+data Pid = Pid {pidName :: String, pidHandle :: Proc.ProcessHandle}
+
+-- | Monad for running shell commands. If a command fails, the entire
+--   computation is aborted unless @mayFail@ is used.
+newtype Shell a = Shell {
+    unSh :: IO ([Pid], Either String a)
+  }
+
+instance Monad Shell where
+  fail err = Shell $ return ([], Left err)
+  return x = Shell $ return ([], Right x)
+  -- | The bind operation of the Shell monad is effectively a barrier; all
+  --   commands on the left hand side of a bind will complete before any
+  --   command on the right hand side is attempted.
+  --   To lazily stream data between two commands, use the @|>@ combinator.
+  (Shell m) >>= f = Shell $ do
+    (pids, x) <- m
+    merr <- waitPids pids
+    case (x, merr) of
+      (Left err, _) -> return ([], Left err)
+      (_, Just err) -> return ([], Left err)
+      (Right x', _) -> unSh (f x')
+
+instance MonadIO Shell where
+  liftIO act = Shell $ flip Ex.catch exHandler $ do
+    x <- act
+    return ([], Right x)
+
+instance Applicative Shell where
+  pure  = return
+  (<*>) = ap
+
+instance Functor Shell where
+  fmap f (Shell x) = Shell (fmap (fmap (fmap f)) x)
+
+-- | Run a Shell computation. The program's working directory and environment
+--   will be restored after after the computation finishes.
+shell :: Shell a -> IO (Either String a)
+shell act = do
+    dir <- Dir.getCurrentDirectory
+    env <- M.fromList <$> Env.getEnvironment
+    (pids, res) <- unSh act
+    merr <- waitPids pids
+    Dir.setCurrentDirectory dir
+    resetEnv env
+    case merr of
+      Just err -> return $ Left err
+      _        -> return res
+  where
+    resetEnv old = do
+      new <- M.fromList <$> Env.getEnvironment
+      mapM_ (Env.unsetEnv . fst) (M.toList (new M.\\ old))
+      mapM_ (uncurry Env.setEnv) (M.toList (M.filterWithKey (changed old) new))
+    changed old k v = maybe True (/= v) (M.lookup k old)
+
+-- | Lazy counterpart to monadic bind. To stream data from a command 'a' to a
+--   command 'b', do 'a |> b'.
+(|>) :: Shell String -> (String -> Shell a) -> Shell a
+(Shell m) |> f = Shell $ do
+  (pids, x) <- m
+  (pids', x') <- case x of
+    Left err -> return ([], Left err)
+    Right x' -> unSh (f x')
+  return (pids ++ pids', x')
+infixl 1 |>
+
+-- | Create a temp directory in the standard system temp directory, do
+--   something with it, then remove it.
+withTempDirectory :: String -> (FilePath -> Shell a) -> Shell a
+withTempDirectory template act = Shell $ do
+    Temp.withSystemTempDirectory template act'
+  where
+    act' fp = Ex.catch (unSh (act fp)) exHandler
+
+-- | Create a temp directory in given directory, do something with it, then
+--   remove it.
+withCustomTempDirectory :: FilePath -> (FilePath -> Shell a) -> Shell a
+withCustomTempDirectory dir act = Shell $ do
+    Temp.withTempDirectory dir "shellmate" act'
+  where
+    act' fp = Ex.catch (unSh (act fp)) exHandler
+
+-- | Create a temp file in the standard system temp directory, do something
+--   with it, then remove it.
+withTempFile :: String -> (FilePath -> IO.Handle -> Shell a) -> Shell a
+withTempFile template act = Shell $ do
+    Temp.withSystemTempFile template act'
+  where
+    act' fp h = Ex.catch (unSh (act fp h)) exHandler
+
+-- | Create a temp file in the standard system temp directory, do something
+--   with it, then remove it.
+withCustomTempFile :: FilePath -> (FilePath -> IO.Handle -> Shell a) -> Shell a
+withCustomTempFile dir act = Shell $ do
+    Temp.withTempFile dir "shellmate" act'
+  where
+    act' fp h = Ex.catch (unSh (act fp h)) exHandler
+
+-- | Perform an action that may fail without aborting the entire computation.
+--   Forces serialization.
+try :: Shell a -> Shell (Either String a)
+try (Shell act) = Shell $ do
+  (pids, x) <- Ex.catch act exHandler
+  merr <- waitPids pids
+  case merr of
+    Just err -> return ([], Right (Left err))
+    _        -> return ([], Right x)
+
+-- | Wait for all processes in the given list. If a process has failed, its
+--   error message is returned and the rest are killed.
+waitPids :: [Pid] -> IO (Maybe String)
+waitPids (p:ps) = do
+  exCode <- Proc.waitForProcess (pidHandle p)
+  case exCode of
+    Exit.ExitFailure ec -> do
+      killPids ps
+      return . Just $ "Command '" ++ (pidName p) ++ "' failed with error "
+                    ++" code " ++ show ec
+    _ -> do
+      waitPids ps
+waitPids _ = do
+  return Nothing
+
+-- | Kill all processes in the list.
+killPids :: [Pid] -> IO ()
+killPids = mapM_ (Proc.terminateProcess . pidHandle)
+
+-- | General exception handler; any exception causes failure.
+exHandler :: Ex.SomeException -> IO ([Pid], Either String a)
+exHandler x = return ([], Left $ show x)
+
+-- | Like 'run', but echoes the command's text output to the screen instead of
+--   returning it.
+run_ :: FilePath -> [String] -> String -> Shell ()
+run_ p args stdin = do
+  exCode <- liftIO $ do
+    (Just inp, _, pid) <- runP p args Proc.CreatePipe Proc.Inherit
+    IO.hPutStr inp stdin
+    IO.hClose inp
+    Proc.waitForProcess pid
+  case exCode of
+    Exit.ExitFailure ec -> fail $ "Command '" ++ p ++ "' failed with error " 
+                                ++" code " ++ show ec
+    _                   -> return ()
+
+-- | Run an interactive process.
+runInteractive :: FilePath -> [String] -> Shell ()
+runInteractive p args = do
+  exCode <- liftIO $ do
+    (_, _, pid) <- runP p args Proc.Inherit Proc.Inherit
+    Proc.waitForProcess pid
+  case exCode of
+    Exit.ExitFailure ec -> fail (show ec)
+    _                   -> return ()
+
+-- | Execute an external command. No globbing, escaping or other external shell
+--   magic is performed on either the command or arguments. The program's text
+--   output will be returned, and not echoed to the screen.
+run :: FilePath -> [String] -> String -> Shell String
+run p args stdin = do
+  (output, pid) <- liftIO $ do
+    (Just inp, Just out, pid) <- runP p args Proc.CreatePipe Proc.CreatePipe
+    let feed str = do
+          case splitAt 4096 str of
+            ([], [])      -> IO.hClose inp
+            (first, str') -> IO.hPutStr inp first >> feed str'
+    Conc.forkIO $ feed stdin
+    output <- IO.hGetContents out
+    return (output, pid)
+  output `seq` Shell $ return ([Pid p pid], Right output)
+
+-- | Create a process. Helper for 'run' and friends.
+runP :: String
+     -> [String]
+     -> Proc.StdStream
+     -> Proc.StdStream
+     -> IO (Maybe IO.Handle, Maybe IO.Handle, Proc.ProcessHandle)
+runP p args stdin stdout = do
+    (inp, out, _, pid) <- Proc.createProcess cproc
+    return (inp, out, pid)
+  where
+    cproc = Proc.CreateProcess {
+        Proc.cmdspec      = Proc.RawCommand p args,
+        Proc.cwd          = Nothing,
+        Proc.env          = Nothing,
+        Proc.std_in       = stdin,
+        Proc.std_out      = stdout,
+        Proc.std_err      = Proc.Inherit,
+        Proc.close_fds    = False,
+#if MIN_VERSION_process(1,2,0)
+        Proc.delegate_ctlc = False,
+#endif
+        Proc.create_group = False
+      }
