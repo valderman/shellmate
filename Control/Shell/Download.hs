13f02948793d14e40faa9909136dbb46495f5211
Anton Ekblad <anton@ekblad.cc>
2015-11-25 02:06:47 +0100

    Replace download-curl with HTTP, since download-curl is broken on Windows.

--- Control/Shell/Download.hs
+++ Control/Shell/Download.hs
@@ -1,4 +1,5 @@
--- | Shellmate wrapper for @download-curl@.
+{-# LANGUAGE FlexibleInstances, OverloadedStrings #-}
+-- | High level functions for downloading files.
 module Control.Shell.Download (
     URI,
     fetch, fetchBytes,
@@ -6,31 +7,53 @@ module Control.Shell.Download (
     fetchTags, fetchXML, fetchFeed
   ) where
 import Data.ByteString as BS (ByteString, writeFile)
-import Network.Curl.Download as C
-import Text.HTML.TagSoup (Tag)
-import Text.XML.Light.Types (Content)
+import Data.String
+import Network.HTTP
+import qualified Network.URI as U
+import Text.Feed.Import (parseFeedString)
 import Text.Feed.Types (Feed)
+import Text.HTML.TagSoup (Tag, parseTags)
+import Text.XML.Light (Content, parseXML)
 import Control.Shell
 
 -- | A Uniform Resource Locator.
 type URI = String
 
-liftE :: IO (Either String a) -> Shell a
+liftE :: Show e => IO (Either e a) -> Shell a
 liftE m = do
   res <- liftIO m
   case res of
-    Left e  -> fail e
+    Left e  -> fail (show e)
     Right x -> return x
+
+httpFail :: (Int, Int, Int) -> String -> Shell a
+httpFail (a,b,c) reason =
+  fail $ "HTTP error " ++ concat [show a, show b, show c] ++ ": " ++ reason
+
+fetchSomething :: (IsString a, HStream a) => URI -> Shell a
+fetchSomething uri = do
+    u <- assert ("could not parse URI `" ++ uri ++ "'") (U.parseURI uri)
+    rsp <- liftE $ simpleHTTP (req u)
+    case rspCode rsp of
+      (2,_,_) -> return (rspBody rsp)
+      code    -> httpFail code (rspReason rsp)
+  where
+    req u = Request {
+        rqURI = u,
+        rqMethod = GET,
+        rqHeaders = [],
+        rqBody = ""
+      }
 
 -- | Download content specified by a url using curl, returning the content
 --   as a strict 'ByteString'.
 fetchBytes :: URI -> Shell ByteString
-fetchBytes = liftE . C.openURI
+fetchBytes = fetchSomething
 
 -- | Download content specified by a url using curl, returning the content
 --   as a 'String'.
 fetch :: URI -> Shell String
-fetch = liftE . C.openURIString
+fetch = fetchSomething
 
 -- | Download content specified by a url using curl, writing the content to
 --   the file specified by the given 'FilePath'.
@@ -40,14 +63,16 @@ fetchFile file = fetchBytes >=> liftIO . BS.writeFile file
 -- | Download the content as for 'fetch', but return it as a list of parsed
 --   tags using the tagsoup html parser.
 fetchTags :: URI -> Shell [Tag String]
-fetchTags = liftE . C.openAsTags
+fetchTags = fmap parseTags . fetch
 
 -- | Download the content as for 'fetch', but return it as parsed XML, using
 --   the xml-light parser.
 fetchXML :: URI -> Shell [Content]
-fetchXML = liftE . C.openAsXML
+fetchXML = fmap parseXML . fetch
 
 -- | Download the content as for 'fetch', but return it as as parsed RSS or
 --   Atom content, using the feed library parser.
 fetchFeed :: URI -> Shell Feed
-fetchFeed = liftE . C.openAsFeed
+fetchFeed uri = do
+  str <- fetch uri
+  assert ("could not parse feed from `" ++ uri ++ "'") (parseFeedString str)
