830206e3a63abe0afcc34bc2ce3d22f4a5f38eeb
Anton Ekblad <anton@ekblad.cc>
2016-08-08 22:59:45 +0200

    Add forkIO, fork2 and fork3, for more low level concurrency.

--- shellmate/Control/Shell/Concurrent.hs
+++ shellmate/Control/Shell/Concurrent.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE CPP #-}
 -- | Concurrency for Shellmate programs.
 module Control.Shell.Concurrent (
-    Future,
+    Future, ThreadId,
+    Control.Shell.Concurrent.forkIO, Control.Shell.Concurrent.killThread,
+    fork2, fork3,
     future, await, check,
     parallel, parallel_,
     chunks
   ) where
-import Control.Concurrent
+import Control.Concurrent as CC
 import Control.Monad
 import Control.Shell
+import Control.Shell.Internal (inEnv)
 import Data.IORef
+import System.Process
+import System.IO (BufferMode (..), hSetBuffering)
 
 -- | Only used to have something reliable to attach the futures' weakrefs to.
 type FinalizerHandle = IORef ThreadId
@@ -35,12 +40,12 @@ future m = do
   env <- getShellEnv
   unsafeLiftIO $ do
     v <- newEmptyMVar
-    tid <- forkIO $ runSh env m >>= putMVar v
+    tid <- CC.forkIO $ runSh env m >>= putMVar v
     -- We need a WeakRef to something that's not referenced by the computation
     -- to be able to kill it when the result is not reachable. IORef to TID is
     -- as good as anything.
     r <- newIORef tid
-    _ <- mkWeakIORef r (killThread tid)
+    _ <- mkWeakIORef r (CC.killThread tid)
     return $ Future r v
 
 -- | Wait for a future value.
@@ -73,3 +78,40 @@ chunks :: Int -> [a] -> [[a]]
 chunks _ []                 = []
 chunks n xs | length xs > n = take n xs : chunks n (drop n xs)
             | otherwise     = [xs]
+
+-- | Run a computation in a separate thread. If the thread throws an error,
+--   it will simply die; the error will not propagate to its parent thread.
+forkIO :: Shell () -> Shell ThreadId
+forkIO m = do
+  env <- getShellEnv
+  unsafeLiftIO $ do
+    CC.forkIO $ void $ runSh env m
+
+-- | Run a computation in a separate thread, with its standard input and output
+--   provided by the first and second handles returned respectively.
+--   The handles are line buffered by default.
+fork2 :: Shell () -> Shell (Handle, Handle, ThreadId)
+fork2 m = do
+  env <- getShellEnv
+  (ri, wi) <- unsafeLiftIO createPipe
+  (ro, wo) <- unsafeLiftIO createPipe
+  unsafeLiftIO $ mapM_ (flip hSetBuffering LineBuffering) [wi,wo]
+  tid <- Control.Shell.Concurrent.forkIO $ do
+    inEnv (env {envStdOut = wo, envStdIn = ri}) m
+  return (wi, ro, tid)
+
+-- | Like 'fork2', but adds a third handle for standard error.
+fork3 :: Shell () -> Shell (Handle, Handle, Handle, ThreadId)
+fork3 m = do
+  env <- getShellEnv
+  (ri, wi) <- unsafeLiftIO createPipe
+  (ro, wo) <- unsafeLiftIO createPipe
+  (re, we) <- unsafeLiftIO createPipe
+  unsafeLiftIO $ mapM_ (flip hSetBuffering LineBuffering) [wi,wo,we]
+  tid <- inEnv (env {envStdOut = wo, envStdErr = we, envStdIn = ri}) $ do
+    Control.Shell.Concurrent.forkIO m
+  return (wi, ro, re, tid)
+
+-- | Terminate a thread spawned by 'Control.Shell.Concurrent.forkIO'.
+killThread :: ThreadId -> Shell ()
+killThread = liftIO . CC.killThread
