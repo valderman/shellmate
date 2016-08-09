be756f85e5622c0a2379815c9399e303cc3b3c1d
Anton Ekblad <anton@ekblad.cc>
2016-08-09 16:24:52 +0200

    Very basic Windows support for Control.Shell.Daemon.

--- shellmate/Control/Shell/Daemon.hs
+++ shellmate/Control/Shell/Daemon.hs
@@ -1,38 +1,55 @@
+{-# LANGUAGE CPP, ForeignFunctionInterface #-}
 -- | Daemonize a shellmate computation.
---   Windows compatibility not guaranteed.
+--   Windows compatibility only with Cygwin.
 module Control.Shell.Daemon (daemonize) where
 import System.Directory
-import System.Environment
 import System.Exit
 import System.IO
-import System.Posix
 import Control.Shell hiding (stdin)
+
+#ifdef WITH_POSIX
+import System.Posix
+#else
+#include "windows_cconv.h"
+foreign import WINDOWS_CCONV unsafe "windows.h FreeConsole"
+        freeConsole :: IO ()
+#endif
 
 -- | Daemonize a shellmate computation. This should be the last thing a
 --   computation does, as this function will terminate the parent computation.
 --   In short, daemonizing a computation involves setting the file creation
 --   mask to 0, closing standard input, output and error file descriptors,
 --   blocking sighup and changing the working directory to @/@.
+--
+--   On Windows without Cygwin, @daemonize@ only detaches the process from the
+--   console in which it is running. On Windows, consider running any program
+--   intended to be deamonized using @START /B your_app@, or better yet,
+--   rewrite it as a Windows service.
+--   Note that any non-service process will still be terminated if the starting
+--   user logs out.
+--
+--   With Cygwin, this function behaves as on non-Windows platforms.
 daemonize :: Shell () -> Shell ()
-daemonize program = do
+#ifdef WITH_POSIX
+daemonize m = do
     env <- getShellEnv
     unsafeLiftIO $ do
-      setFileCreationMask 0
-      forkProcess (p env)
+      void $ setFileCreationMask 0
+      void $ forkProcess (p env)
       exitImmediately ExitSuccess
   where
     p env = do
-      createSession
-      forkProcess (p' env)
+      void $ createSession
+      void $ forkProcess (p' env)
       exitImmediately ExitSuccess
     p' env = do
       changeWorkingDirectory "/"
-      null <- openFd "/dev/null" ReadWrite Nothing defaultFileFlags
+      devnull <- openFd "/dev/null" ReadWrite Nothing defaultFileFlags
       mapM_ closeFd [stdInput, stdOutput, stdError]
-      mapM_ (dupTo null) [stdInput, stdOutput, stdError]
-      installHandler sigHUP Ignore Nothing
+      mapM_ (dupTo devnull) [stdInput, stdOutput, stdError]
+      void $ installHandler sigHUP Ignore Nothing
       dir <- getCurrentDirectory
-      res <- flip runSh program $ env
+      res <- flip runSh m $ env
         { envStdIn   = stdin
         , envStdOut  = stdout
         , envStdErr  = stderr
@@ -42,3 +59,6 @@ daemonize program = do
       case res of
         Left (Failure _) -> exitFailure
         _                -> exitSuccess
+#else
+daemonize m = freeConsole >> m
+#endif
